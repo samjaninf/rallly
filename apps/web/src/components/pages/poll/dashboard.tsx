4a8a85cc3ecbc156c7e47b1289a7651653a44f9f
Luke Vella <me@lukevella.com>
2023-04-28 18:34:44 +0100

    Clean up

--- apps/web/src/components/pages/poll/dashboard.tsx
+++ apps/web/src/components/pages/poll/dashboard.tsx
@@ -1,16 +1,8 @@
-import { InboxIcon } from "@rallly/icons";
-
 import { MostPopularDates } from "@/components/pages/poll/dashboard/most-popular-dates";
 import { ParticipantLink } from "@/components/pages/poll/dashboard/participant-link";
 import { RecentlyVoted } from "@/components/pages/poll/dashboard/recently-voted";
-import {
-  useCurrentEvent,
-  useCurrentPollResponses,
-} from "@/contexts/current-event";
 
 export const Dashboard = () => {
-  const { data: responses = [] } = useCurrentPollResponses();
-  const { data: poll } = useCurrentEvent();
   return (
     <div className="space-y-4">
       <ParticipantLink />
